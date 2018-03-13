ff1afe385628df3d6b640177bc7a8c01cb8a11b2
Piers <pierswalter3@googlemail.com>
2018-03-13 23:27:14 +0000

    Initial Serial working with indicators turning on and off

--- WRAndroid/app/src/main/java/com/walterrivett/wrandroid/MainActivity.java
+++ WRAndroid/app/src/main/java/com/walterrivett/wrandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -16,9 +17,15 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.github.anastr.speedviewlib.RaySpeedometer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
     LocationManager locationManager;
@@ -29,12 +36,18 @@ public class MainActivity extends Activity {
 
     private static int kFineLocation = 1000;
 
+    private ImageView indicatorImage;
+
+    private BluetoothSerial bluetoothSerial;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mContext = this;
+
+        indicatorImage = findViewById(R.id.indicatorImage);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new speed();
@@ -61,8 +74,93 @@ public class MainActivity extends Activity {
             }
         }, 1300);
 
+        bluetoothSerial = new BluetoothSerial(this, new BluetoothSerial.MessageHandler() {
+            @Override
+            public int read(int bufferSize, byte[] buffer) throws IOException, InterruptedException {
+                Log.d("Bluetooth Handler","Received Message");
+                StringBuilder outputString = new StringBuilder();
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                byte[] outputBytes;
+
+                for(int i=0;i<4;i++){
+                    int readByte = bluetoothSerial.read();
+                    outputString.append(Integer.toHexString(readByte));
+                    outputString.append(",");
+                    output.write(readByte);
+                }
+
+                outputBytes = output.toByteArray();
+                Log.d("Bluetooth Handler", "Message Hex: "+outputString);
+
+                processBytes(outputBytes);
+
+                return 0;
+
+            }
+        }, "Pi");
+
+        bluetoothSerial.connect();
 
     }
+
+    private void processBytes(byte[] bytes){
+        if(bytes.length == 4 && bytes[3] == -1 /* 0xFF */){
+            Log.d("Process Bytes","Received Correct Byte Count");
+            //Indicators
+            if(bytes[0]==1){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showIndicators();
+                    }
+                });
+
+            }else{
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        hideIndicators();
+                    }
+                });
+            }
+
+            //Neutral
+            if(bytes[1]==1){
+                showNeutral();
+            }else{
+                hideNeutral();
+            }
+
+            //Hi-Beam
+            if(bytes[2]==1){
+                showHiBeam();
+            }else{
+                hideHiBeam();
+            }
+        }
+    }
+
+    private void showHiBeam() {
+
+    }
+    private void hideHiBeam(){
+
+    }
+
+    private void hideNeutral() {
+    }
+
+    private void showIndicators(){
+        indicatorImage.setVisibility(View.VISIBLE);
+    }
+    private void hideIndicators(){
+        indicatorImage.setVisibility(View.INVISIBLE);
+    }
+
+    private void showNeutral() {
+
+    }
+
 
     private class speed implements LocationListener{
         @Override
@@ -97,5 +195,9 @@ public class MainActivity extends Activity {
         }else{
             Toast.makeText(this,"This App Needs Location Permission to Work",Toast.LENGTH_LONG);
         }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 }
